e2bf7a4a8b95f6bf0489b15150eeac16ec2b6951
auth1ery <skunkolee@gmail.com>
2025-10-24 13:27:08 -0600

    Delete reading_mode.js

--- reading_mode.js
+++
@@ -1,9 +0,0 @@
-// reading_mode.js
-(function() {
-  console.log("[Ext] Reading Mode enabled");
-  document.querySelectorAll("header, footer, nav, aside, iframe, video, img, button").forEach(e => e.remove());
-  document.body.style.maxWidth = "700px";
-  document.body.style.margin = "40px auto";
-  document.body.style.fontSize = "20px";
-  document.body.style.lineHeight = "1.6";
-})();
